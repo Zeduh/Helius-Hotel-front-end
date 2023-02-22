a004f8246edb7440cf8509a9a1eb999a10a73444
Eduardo Soares Dutra <eduardosoaresdutra@gmail.com>
2023-02-22 10:41:03 -0300

    Adicionado component 'FooterComponent' e inserido na HomeView

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <main>
-      <Header />
+      <HeaderComponent />
       <h1>Homeee13123e</h1>
+      <FooterComponent />
   </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import Header from "@/components/HeaderComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+import HeaderComponent from "@/components/HeaderComponent.vue";
 
 export default {
   name: "HomeView",
   components: {
-    Header,
-  },
+    HeaderComponent,
+    FooterComponent
+},
 };
 </script>
